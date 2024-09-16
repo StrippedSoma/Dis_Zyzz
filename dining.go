822355b8a77c977d364b1d5fbd6e345c74a00477
StrippedSoma <simun.rasmussen@gmail.com>
2024-09-16 12:36:34 +0200

    lavet bool 2-vejs kommunikation og waitgroup

--- dining.go
+++ dining.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 func main() {
 	t := time.Now()
+	var wg sync.WaitGroup
 	var eatAmount int = 3
 	chan1 := make(chan bool)
 	chan2 := make(chan bool)
@@ -21,6 +23,7 @@ func main() {
 	go fork(chan4)
 	go fork(chan5)
 
+<<<<<<< Updated upstream
 	go philosopher(chan1, chan2, eatAmount, t)
 	go philosopher(chan2, chan3, eatAmount, t)
 	go philosopher(chan3, chan4, eatAmount, t)
@@ -42,6 +45,50 @@ func fork(channel chan bool) {
 				channel <- true
 			} else {
 				taken = false
+=======
+	go fork(chanIn1, chanOut1, chanOut2)
+	go fork(chanIn2, chanOut3, chanOut4)
+	go fork(chanIn3, chanOut5, chanOut6)
+	go fork(chanIn4, chanOut7, chanOut8)
+	go fork(chanIn5, chanOut9, chanOut10)
+
+	wg.Add(5)
+
+	go philosopher(chanIn1, chanIn2, chanOut1, chanOut4, eatAmount, t)
+	go philosopher(chanIn2, chanIn3, chanOut3, chanOut6, eatAmount, t)
+	go philosopher(chanIn3, chanIn4, chanOut5, chanOut8, eatAmount, t)
+	go philosopher(chanIn4, chanIn5, chanOut7, chanOut10, eatAmount, t)
+	go philosopher(chanIn5, chanIn1, chanOut9, chanOut2, eatAmount, t)
+
+	wg.Wait()
+	fmt.Println("Done")
+}
+
+func fork(listen chan bool, tellRight chan bool, tellLeft chan bool) {
+	onTable := true
+	side := true
+	for {
+		request := <-listen
+		if request && onTable {
+			tellRight <- true
+			onTable = false
+			side = true
+		} else if !request && onTable {
+			tellLeft <- true
+			onTable = false
+			side = false
+		} else if request && !onTable {
+			if side {
+				onTable = true
+			} else {
+				tellRight <- false
+			}
+		} else if !request && !onTable {
+			if !side {
+				onTable = true
+			} else {
+				tellLeft <- false
+>>>>>>> Stashed changes
 			}
 		} else {
 			channel <- false
@@ -49,6 +96,7 @@ func fork(channel chan bool) {
 	}
 }
 
+<<<<<<< Updated upstream
 func philosopher(left chan bool, right chan bool, eatAmount int, t time.Time) {
 	//fmt.Println("Philosopher is thinking")
 	for eatAmount > 0 {
@@ -59,11 +107,27 @@ func philosopher(left chan bool, right chan bool, eatAmount int, t time.Time) {
 				//fmt.Println("Philosopher is eating", eatAmount)
 				left <- false
 				right <- false
+=======
+func philosopher(tellLeft chan bool, tellRight chan bool, listenLeft chan bool, listenRight chan bool, eatAmount int, t time.Time) {
+	//fmt.Println("Philosopher is thinking")
+	for eatAmount > 0 {
+		tellLeft <- true
+		if <-listenLeft == true {
+			tellRight <- false
+			if <-listenRight == true {
+				//fmt.Println("Philosopher is eating", eatAmount)
+				tellLeft <- true
+				tellRight <- false
+>>>>>>> Stashed changes
 				time.Sleep(1 * time.Millisecond)
 				eatAmount--
 				//fmt.Println("Philosopher is thinking")
 			} else {
+<<<<<<< Updated upstream
 				right <- false
+=======
+				tellLeft <- true
+>>>>>>> Stashed changes
 				time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
 			}
 		}
