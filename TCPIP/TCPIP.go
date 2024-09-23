3dcff6cedc130b89c627897c9a76df77ef731533
Símun Larsson Løkke Rasmussen <simra@itu.dk>
2024-09-23 21:56:50 +0200

    Replaced waitgroup with locked channel

--- TCPIP/TCPIP.go
+++ TCPIP/TCPIP.go
@@ -2,35 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
 func main() {
-	var wg sync.WaitGroup
 	channel1 := make(chan int)
 	channel2 := make(chan int)
 	channel3 := make(chan int, 2)
-	wg.Add(2)
+
 	go func() {
-		defer wg.Done()
 		server(channel2, channel1, channel3)
 	}()
 
 	go func() {
-		defer wg.Done()
 		client(channel1, channel2, channel3)
 	}()
-	
-	var x = <- channel3
+
+	var x = <-channel3
 	if x == 1 {
-		fmt.Println("client spazzed out")
+		fmt.Println("client timed out")
 	} else if x == 2 {
-		fmt.Println("server spazzed out")
+		fmt.Println("server timed out")
 	} else if x == 3 {
 		fmt.Println("TCPIP SUCCESSFUL")
 	}
-	wg.Wait()
+
 }
 
 func client(chanOut chan int, chanIn chan int, toMain chan int) {
@@ -38,7 +34,7 @@ func client(chanOut chan int, chanIn chan int, toMain chan int) {
 	var attempts = 0
 	for done == false {
 		if attempts == 3 {
-			fmt.Println("client timed out")
+			fmt.Println("Client Error - 3 tries")
 			toMain <- 1
 			break
 		}
@@ -52,57 +48,57 @@ func client(chanOut chan int, chanIn chan int, toMain chan int) {
 				sSeq = <-chanIn + 1
 				chanOut <- sSeq
 				done = true
-				fmt.Println("SUCCESS")
-				toMain <- 0
+				fmt.Println("Client recieved")
 			} else {
-				fmt.Println("server response not valid, resending")
+				fmt.Println("Server response not valid, resending")
 				attempts++
 			}
 		case <-time.After(1 * time.Second):
-			fmt.Println("server timed out, resending")
+			fmt.Println("Server timed out, resending")
 			attempts++
 		}
 
 	}
-	fmt.Println("LOOP DONE")
+	fmt.Println("Client Loop Done")
 }
 
 func server(chanOut chan int, chanIn chan int, toMain chan int) {
 	var done = false
+	var sent = false
 	var attempts = 0
+	var cSeq int = <-chanIn
+
 	for done == false {
 		var sSeq int = 300
-		var cSeq int
 		if attempts == 3 {
-			fmt.Println("server timed out")
+			fmt.Println("Server Error - 3 tries")
 			toMain <- 2
 			break
 		}
-		select {
-		case clientresponse := <-chanIn:
-			cSeq = clientresponse
-			fmt.Println("SYNACK")
+		if !sent {
+			fmt.Println("Server responding")
 			chanOut <- cSeq + 1
 			chanOut <- sSeq
-		case <-time.After(1 * time.Second):
-			fmt.Println("client timed out, resending")
-			attempts++
-			continue
+			sent = true
 		}
-		select {
-		case clientresponse := <-chanIn:
-			if sSeq+1 == clientresponse {
-				fmt.Println("SUCCESS")
-				done = true
-				toMain <- 0
-			} else {
-				fmt.Println("FAILURE")
+		if sent {
+			select {
+			case clientresponse := <-chanIn:
+				if sSeq+1 == clientresponse {
+					fmt.Println("Server recieved - done")
+					done = true
+					toMain <- 3
+				} else {
+					fmt.Println("Client response not valid, resending")
+					attempts++
+					sent = false
+				}
+			case <-time.After(1 * time.Second):
+				fmt.Println("Client timed out, resending")
+				attempts++
+				sent = false
 			}
-		case <-time.After(1 * time.Second):
-			fmt.Println("client timed out, resending")
-			cSeq = 0
-			attempts++
 		}
 	}
-	fmt.Println("LOOP DONE")
+	fmt.Println("Server Loop Done")
 }
